612270a0ab8794b58a167b362e3c08ca35b3740b
Alexander <gasha.git@gmail.com>
2023-05-19 21:22:54 +0300

    Added html

--- app/src/main/java/com/idutvuk/go_maf/RulesFragment.kt
+++ app/src/main/java/com/idutvuk/go_maf/RulesFragment.kt
@@ -7,32 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import com.idutvuk.go_maf.databinding.FragmentRulesBinding
 
 class RulesFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RulesFragment()
-
-        var string = "<h2>Sdoba</h2><br><p>revolver</p><br>"
-
-    }
-
-    private lateinit var viewModel: RulesViewModel
-    lateinit var b: FragmentRulesBinding
-            override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        b = FragmentRulesBinding.inflate(inflater,container,false)
-        b.tvHtmlViewer.text = Html.fromHtml(string,Html.FROM_HTML_MODE_COMPACT)
-        return b.root
+        val view = inflater.inflate(R.layout.fragment_rules, container, false)
+        val webView = view.findViewById<WebView>(R.id.webView)
+        webView.loadUrl("file:///android_asset/index.html")
+        return view
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this)[RulesViewModel::class.java]
-        // TODO: Use the ViewModel
-    }
-
 }
