69cbaaf9da51180ac1ade7bc9295ba25e8b1dd15
Alexander <gasha.git@gmail.com>
2023-05-31 00:08:24 +0300

    Old files deleted

--- app/src/main/java/com/idutvuk/go_maf/legacy/GameMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.idutvuk.go_maf.legacy
-
-import com.idutvuk.go_maf.model.gamedata.EventImportance
-@Deprecated("Use compose")
-data class GameMessage (
-    var heading: String = "EMPTY HEADING",
-    var description: String = "EMPTY TEXT",
-    var importance: EventImportance // = EventImportance.REGULAR
-)
