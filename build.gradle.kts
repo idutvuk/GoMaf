43d327e34fe13a96f627ebec4685dc6cd4ef95b2
Alexander <idutvuk@gmail.com>
2026-01-05 13:23:51 +0300

    remove gradle

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "1.8.0-1.0.8" apply false
-//    id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
-}
